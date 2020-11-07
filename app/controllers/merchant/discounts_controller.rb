ca7bd048ddf5c7fc0440f033297f6970dad0e214
Kevin Cuadros <kev040798@gmail.com>
2020-11-07 17:32:15 -0500

    Create the index action

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,5 +1,25 @@
 class Merchant::DiscountsController < Merchant::BaseController
+  def index
+    @discounts = Discount.where(merchant_id: current_user.merchant.id)
+  end
+
   def new
 
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.quantity >= 5 && discount.percent < 1.00 && discount.percent > 0.00
+      discount.save
+      redirect_to '/merchant/discounts'
+    else
+      flash[:error] = 'Could not create discount: quantity less than 5 or percent is incorrect'
+      redirect_to '/merchant/discounts/new'
+    end
+  end
+
+  def discount_params
+    params.permit(:percent, :quantity)
   end
 end
